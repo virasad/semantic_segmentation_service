2b67ca5ddb1cf3ee06fbb0387a710a1f5666d302
Amir Sharifi <ami_rsh@outlook.com>
2022-03-12 19:52:52 +0330

    train added

---
+++ train/main.py
@@ -0,0 +1,63 @@
+from typing import Optional
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+import train as tr
+from inference.predict import InferenceSeg
+import base64
+import cv2
+import numpy as np
+import pickle
+
+
+
+
+app = FastAPI()
+detector = None
+detector = InferenceSeg()
+
+class Train(BaseModel):
+    images: str
+    annotation: str
+    save_name: Optional[str] = None
+    batch_size: Optional[int] = None
+    num_dataloader_workers: Optional[int] = None
+    epochs: Optional[int] = None
+
+class Predict(BaseModel):
+    image_path: str
+
+
+def im2json(image):
+    """Convert a Numpy array to JSON string"""
+    imdata = pickle.dumps(image)
+    return base64.b64encode(imdata).decode('ascii')
+
+@app.post('/set_model/')
+def set_model(model_path: str = None):
+    try:
+        detector.load_model(model_path)
+        return {'status': 'success'}
+    except Exception as e:
+        return {'status': 'error', 'error': str(e)}
+
+
+@app.post("/train/")
+def read_train(train: Train = None):
+    try:
+        result = tr.train_from_coco(train.images, train.annotation, train.save_name, train.batch_size, train.num_dataloader_workers, train.epochs)
+        return result
+
+    except Exception as e:
+        return {"result": "failed", 'error': str(e)}
+
+@app.post('/predict/')
+def predict(predict: Predict = None):
+    try:
+        result = detector.detect_add_to_image(predict.image_path)
+        img_str = im2json(result)
+        print(img_str)
+        return {"result": "success", 'image': img_str}
+
+    except Exception as e:
+        return {"result": "failed", 'error': str(e)}
---
+++ train/readme.md
@@ -0,0 +1,31 @@
+# Installation
+First of all please install the dependencies:
+- [pytorch](https://pytorch.org/get-started/locally/)
+- [Flash images](https://lightning-flash.readthedocs.io/en/latest/installation.html)
+- Flash:
+
+`pip install lightning-flash`
+## Addition for webservice
+
+```bash
+pip install fastapi
+pip install "uvicorn[standard]"
+```
+
+```bash
+pip install 'lightning-flash[image]'
+```
+- other dependencies:
+```bash
+pip install -r requirements.txt 
+```
+
+# How to run?
+## run from script
+
+```bash
+cd backbone
+uvicorn main:app --reload
+```
+## See API doc
+http://127.0.0.1:8000/docs
