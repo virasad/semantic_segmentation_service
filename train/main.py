bd46ed6836d293d1a345521fea6f5b292fd2d0c5
Amir Sharifi <am.sharifi1995@gmail.com>
2022-04-15 11:26:44 +0430

    inference main file changed

--- train/main.py
+++ train/main.py
@@ -25,20 +25,18 @@ class Train(BaseModel):
 
 @app.post("/train/")
 def read_train(train: Train = None):
-        # print(train)
-    # try:
+    try:
         result = tr.train_from_coco(train.images, train.annotation, train.save_name, int(train.batch_size),
                                     int(train.num_dataloader_workers), int(train.epochs), int(train.num_classes),
                                     float(train.validation_split))
-        print('yeesssssssssss',result)
         response_url = os.environ.get('RESPONSE_URL','http://127.0.0.1:8000/api/v1/train/done')
         a = requests.post(response_url, data={**result, **train.extra_kwargs, 'save_name': train.save_name +'_model.pt'})
-        print('response',a.text)
+        print(a.text)
         return result
 
-    # except Exception as e:
-    #     return {"result": "failed", 'error': str(e)}
+    except Exception as e:
+        return {"result": "failed", 'error': str(e)}
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=int(os.environ.get('PORT', '5554')))
+    uvicorn.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', '5554')))
